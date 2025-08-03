83dba0c8097ca29998d695c189b54a2b1bfa2d0a
Dylan <lightfury954@gmail.com>
2025-08-03 05:19:46 -0400

    fixed some of the logic for parsing arguments, still need to make main script work properly

--- myparser/parser.py
+++ myparser/parser.py
@@ -1,15 +1,32 @@
 import argparse
+print("this is going first")
 
 def run_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("command")
     parser.add_argument("--description")
     parser.add_argument("--amount")
-    
 
     args = parser.parse_args()
-    return args
+    final_args = get_parse(args)
+    return final_args
 
 
-if __name__ == "__main__":
-    run_parser() 
+def get_parse(args):
+        if args.command == "add":
+            if args.description:
+                if args.amount:
+                    return args
+                else:
+                    missing_key("amount")
+            else:
+                missing_key("description")
+
+
+def missing_key(text):
+    print(f"missing {text}, add {text} using --{text} if you would like to add an expense")
+
+
+final_args = run_parser()
+print(final_args)
+
